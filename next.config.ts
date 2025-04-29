1ddd321e39fbce218fad6281e3206049dafc7ff4
krishnakumarphynlabz <krishna.kumar@phynlabz.com>
2025-04-29 15:26:49 +0530

    changes of next.config.js image related

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // allows all domains over HTTPS
+      },
+      {
+        protocol: "http",
+        hostname: "**", // allows all domains over HTTP
+      },
+    ],
   },
 };
 
